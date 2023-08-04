144e78715f72072b534293a45e00cea9f309a78a
allogic <mial.burg@gmail.com>
2023-08-04 14:15:58 +0200

    working LLVM build on linux

--- build-llvm.ps1
+++ build-llvm.ps1
@@ -1,8 +1,8 @@
-Push-Location llvm-project
+Push-Location llvm
     Remove-Item -LiteralPath "build" -Force -Recurse
     New-Item -ItemType "directory" -Path "build"
 	Push-Location build
-        & "C:\Program Files\CMake\bin\cmake.exe" -A x64 -D"LLVM_ENABLE_PROJECTS=clang;lld" -D"CMAKE_BUILD_TYPE=Release" -D"CMAKE_INSTALL_PREFIX=C:\Program Files\llvm" -G "Visual Studio 17 2022" ../llvm
+        & "C:\Program Files\CMake\bin\cmake.exe" -D"LLVM_ENABLE_PROJECTS=clang;lld" -D"CMAKE_GENERATOR_PLATFORM=x64" -D"CMAKE_BUILD_TYPE=Release" -D"CMAKE_INSTALL_PREFIX=C:\Program Files\llvm" -G "Visual Studio 17 2022" ../llvm
         & "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe" LLVM.sln /p:Configuration=Release
 	Pop-Location
 Pop-Location
--- build-llvm.sh
+++ build-llvm.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-pushd llvm-project
+pushd llvm
 	rm -rf build
 	mkdir build
 	pushd build
-		cmake -A x64 -D"LLVM_ENABLE_PROJECTS=clang;lld" -D"CMAKE_BUILD_TYPE=Release" -D"CMAKE_INSTALL_PREFIX=/opt/llvm" -G "Unix Makefiles" ../llvm
-		make
+		cmake -D"LLVM_ENABLE_PROJECTS=clang;lld" -D"CMAKE_BUILD_TYPE=Release" -D"CMAKE_INSTALL_PREFIX=/opt/llvm" -G "Unix Makefiles" ../llvm
+		make -j 8
 	popd
 popd
