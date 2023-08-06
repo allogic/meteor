d16314a3e967163955facad38516bae2a5b2250b
allogic <mial.burg@gmail.com>
2023-08-07 01:38:08 +0200

    Remove msbuild.exe

--- build_llvm.ps1
+++ build_llvm.ps1
@@ -5,8 +5,7 @@ Push-Location llvm
     New-Item -ItemType "directory" -Path "build"
 	Push-Location build
         cmake -D"LLVM_ENABLE_PROJECTS=clang;lld;lldb" -D"CMAKE_BUILD_TYPE=Release" -G "Visual Studio 17 2022" ../llvm
-        #cmake --build .
-        msbuild LLVM.sln /p:Configuration=Release
+        cmake --build .
         Copy-Item -Path "Release\bin" -Destination "..\..\clang" -Recurse
 	Pop-Location
 Pop-Location
--- setup_environment.ps1
+++ setup_environment.ps1
@@ -1,5 +1,3 @@
-$msbuild = "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin"
 $clang = "$(Get-Location)\clang"
 
 $env:PATH += ";$clang"
-$env:PATH += ";$msbuild"
