4b6b1570ab8d5fe9e05411543b22a37ba0588da3
allogic <mial.burg@gmail.com>
2023-08-12 14:40:07 +0200

    Build system cleanup

---
+++ llvm.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import shutil
+import platform
+
+def configure_llvm():
+	llvm_path=f'llvm/llvm'
+	build_path=f'llvm/build'
+	if platform.system() == 'Windows':
+		install_path=f'C:/llvm'
+	elif platform.system() == 'Linux':
+		install_path=f'/opt/llvm'
+	if os.path.exists(build_path):
+		shutil.rmtree(build_path)
+	os.mkdir(build_path)
+	os.system(f'cmake -D LLVM_ENABLE_PROJECTS=clang;lld;lldb -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX={install_path} -G "Unix Makefiles" -S {llvm_path} -B {build_path}')
+
+def build_llvm():
+	build_path=f'llvm/build'
+	os.system(f'cmake --build {build_path} --parallel 8')
+
+def install_llvm():
+	build_path=f'llvm/build'
+	os.system(f'cmake --install {build_path}')
+
+cmd=sys.argv[1]
+
+if 'c' in cmd:
+	configure_llvm()
+
+if 'b' in cmd:
+	build_llvm()
+
+if 'i' in cmd:
+	install_llvm()
---
+++ project.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import shutil
+import platform
+
+if platform.system() == 'Windows':
+	os.environ['PATH'] += os.pathsep + 'C:/llvm/bin'
+elif platform.system() == 'Linux':
+	os.environ['PATH'] += os.pathsep + '/opt/llvm/bin'
+
+def configure_project(project, configuration):
+	project_path=f'projects/{project}'
+	build_path=f'projects/{project}/build'
+	if os.path.exists(build_path):
+		shutil.rmtree(build_path)
+	os.mkdir(build_path)
+	os.system(f'cmake -D CMAKE_C_COMPILER=clang -D CMAKE_BUILD_TYPE={configuration} -G "Unix Makefiles" -S {project_path} -B {build_path}')
+
+def build_project(project):
+	build_path=f'projects/{project}/build'
+	os.system(f'cmake --build {build_path} --parallel 8')
+
+def run_project(project):
+	build_path=f'projects/{project}/build'
+	os.chdir(build_path)
+	os.system(f'{project}')
+
+def debug_project(project):
+	build_path=f'projects/{project}/build'
+	os.chdir(build_path)
+	os.system(f'lldb {project}')
+
+def build_shader(project):
+	shader_path=f'projects/{project}/shaders'
+	if os.path.exists(shader_path):
+		for file_name in os.listdir(shader_path):
+			file_stem,file_ext=os.path.splitext(file_name)
+			file_path=f'{shader_path}/{file_stem}'
+			if file_ext=='.vert' or file_ext=='.frag' or file_ext=='.comp':
+				os.system(f'glslc {file_path}{file_ext} -o {file_path}{file_ext}.spv')
+
+cmd=sys.argv[1]
+
+if 'c' in cmd:
+	project = sys.argv[2]
+	configuration = sys.argv[3]
+	configure_project(project, configuration)
+
+if 'b' in cmd:
+	project = sys.argv[2]
+	build_project(project)
+
+if 'r' in cmd:
+	project = sys.argv[2]
+	run_project(project)
+
+if 'd' in cmd:
+	project = sys.argv[2]
+	debug_project(project)
+
+if 's' in cmd:
+	project = sys.argv[2]
+	build_shader(project)
